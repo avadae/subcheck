2a6f106e19bc3d188bc43d1df7841a240a3f9dd1
grrava <alex.vandenabeele@gmail.com>
2022-07-23 14:54:56 +0200

    [ava] removed trimming, no longer possible in .net 6.0 with serialized types

--- XmlSerialization.cs
+++ XmlSerialization.cs
@@ -8,43 +8,35 @@ namespace SubCheck
 	{
 	    public static void Serialize<T>(string filename, T item)
 	    {
-			using (var textWriter = new XmlTextWriter(filename, Encoding.ASCII))
-			{
-				textWriter.Formatting = Formatting.Indented;
-				var serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
-				serializer.Serialize(textWriter, item);
-			}
-	    }
+			using var textWriter = new XmlTextWriter(filename, Encoding.ASCII);
+			textWriter.Formatting = Formatting.Indented;
+			var serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+			serializer.Serialize(textWriter, item);
+		}
 	
 	    public static T Deserialize<T>(string filename)
 	    {
-	        using (StreamReader sr = new StreamReader(filename))
-	        {
-	            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
-	            return (T)serializer.Deserialize(sr);
-	        }
-	    }
+			using var sr = new StreamReader(filename);
+			var serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+			return (T)serializer.Deserialize(sr);
+		}
 		
 	    public static T DeserializeXml<T>(string xml)
 	    {
 			T result;
-	        using (StreamReader sr = new StreamReader(new MemoryStream(Encoding.Default.GetBytes(xml))))
-	        {
-	            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
-	            result = (T)serializer.Deserialize(sr);
-	        }
+			using var sr = new StreamReader(new MemoryStream(Encoding.Default.GetBytes(xml)));
+	        var serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+	        result = (T)serializer.Deserialize(sr);
 			return result;
 	    }
 
 		public static string SerializeToXml<T>(T item)
 		{
 			string result;
-			using (var textWriter = new StringWriter())
-			{
-				var serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
-				serializer.Serialize(textWriter, item);
-				result = textWriter.ToString();
-			}
+			using var textWriter = new StringWriter();
+			var serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+			serializer.Serialize(textWriter, item);
+			result = textWriter.ToString();
 			return result;
 		}
 	}
--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,2 @@
-dotnet publish -r win-x64 -p:PublishTrimmed=True -p:TrimMode=CopyUsed -c Release /p:PublishSingleFile=true "subcheck.csproj" 
+dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true "subcheck.csproj" 
 Copy-Item ".\bin\Release\net5.0\win-x64\publish\subcheck.exe" .\
