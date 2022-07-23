c0094e0991222f44e7aabd7c322ae2bee92cb99e
grrava <alex.vandenabeele@gmail.com>
2022-07-23 16:56:17 +0200

    introduced gitversion

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,6 @@
+dotnet tool install --global GitVersion.Tool --version 5.*
+dotnet tool install -g dotnet-setversion
+$gitversion = dotnet-gitversion | ConvertFrom-Json 
+setversion $gitversion.AssemblySemVer
 dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true "subcheck.csproj" 
-Copy-Item ".\bin\Release\net5.0\win-x64\publish\subcheck.exe" .\
+Copy-Item ".\bin\Release\net6.0\win-x64\publish\SubCheck.exe" .\
