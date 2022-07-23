6c32ace9061d942334e6ed661b33c2e4f96b4c74
grrava <alex.vandenabeele@gmail.com>
2022-07-23 18:22:56 +0200

    quit install log

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
-dotnet tool install --global GitVersion.Tool --version 5.*
-dotnet tool install -g dotnet-setversion
+dotnet tool install --global GitVersion.Tool --version 5.* -v Quiet 
+dotnet tool install -g dotnet-setversion -v Quiet 
 $gitversion = dotnet-gitversion | ConvertFrom-Json 
 setversion $gitversion.AssemblySemVer
 dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true "SubCheck.csproj" 
