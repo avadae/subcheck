e1d70fcdc85153afdf6b1a826fbb06083bac585f
grrava <alex.vandenabeele@gmail.com>
2022-07-23 17:32:55 +0200

    [ava] back to ubuntu

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,5 @@
-# assumes this install: dotnet tool install --global GitVersion.Tool --version 5.*
+dotnet tool install --global GitVersion.Tool --version 5.*
 dotnet tool install -g dotnet-setversion
-echo dotnet-gitversion
 $gitversion = dotnet-gitversion | ConvertFrom-Json 
 setversion $gitversion.AssemblySemVer
 dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true "subcheck.csproj" 
