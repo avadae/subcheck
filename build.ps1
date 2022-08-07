1526b86744511e898563a78a46b5d2082df9ad3c
grrava <alex.vandenabeele@gmail.com>
2022-08-07 20:34:11 +0200

    build fix 3

--- build.ps1
+++ build.ps1
@@ -1,13 +1,10 @@
 dotnet tool install -g GitVersion.Tool --version 5.* -v q
 $gitversion = dotnet-gitversion /updateassemblyinfo | ConvertFrom-Json 
 
-msbuild SubCheck.csproj -t:restore /property:Configuration=Release
-
-msbuild SubCheck.csproj /property:Configuration=Release
+msbuild -t:restore /property:Configuration=Release
+msbuild /property:Configuration=Release
 
 Install-Package ilmerge -RequiredVersion 3.0.29 -Force -SkipValidate -Scope CurrentUser
-#$gitversion = dotnet-gitversion | ConvertFrom-Json 
-#setversion $gitversion.AssemblySemVer subcheck.csproj
 
 $APP_NAME = "subcheck.exe"
 $ILMERGE_BUILD = "Release"
@@ -19,6 +16,3 @@ $ILMERGE_PATH = "$env:USERPROFILE\.nuget\packages\ilmerge\$ILMERGE_VERSION\tools
 echo $ILMERGE_PATH
 echo $ilmergeArgs
 Start-Process $ILMERGE_PATH -ArgumentList $ilmergeArgs -Wait
-
-#dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true "SubCheck.csproj" 
-#Copy-Item "./bin/Release/net6.0/win-x64/publish/SubCheck.exe" .\
