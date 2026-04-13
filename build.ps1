207b722c9c764eadc9980211a6abac569e702aa7
grrava <alex.vandenabeele@gmail.com>
2026-04-13 16:11:55 +0200

    Attempt at build fix

--- build.ps1
+++ build.ps1
@@ -6,10 +6,12 @@ echo $GITVERSION
 dotnet msbuild /property:Configuration=Release
 
 $ILMERGE_VERSION = "3.0.29"
-Install-Package ilmerge -RequiredVersion $ILMERGE_VERSION -Force -SkipValidate -Scope CurrentUser
-
-$APP_NAME = "subcheck.exe"
 $ILMERGE_BUILD = "Release"
 $ILMERGE_ARGS = "Bin\$ILMERGE_BUILD\$APP_NAME /lib:Bin\$ILMERGE_BUILD\ /out:$APP_NAME Microsoft.Build.Locator.dll"
-$ILMERGE_PATH = "$env:USERPROFILE\.nuget\packages\ilmerge\$ILMERGE_VERSION\tools\net452\ILMerge.exe"
+$APP_NAME = "subcheck.exe"
+
+Install-Package ilmerge -RequiredVersion $ILMERGE_VERSION -Force -SkipValidate -Scope CurrentUser
+$pkg = Get-Package ilmerge -RequiredVersion $ILMERGE_VERSION
+$ILMERGE_PATH = Join-Path $pkg.Source "tools\net452\ILMerge.exe"
+
 Start-Process $ILMERGE_PATH -ArgumentList $ILMERGE_ARGS -Wait
